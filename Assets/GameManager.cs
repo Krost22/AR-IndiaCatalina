d2a72f356458b83a0a86d928ab8b3b83316a675d
Eduardo De Jesús Mogollón Salcedo <eduardomogollonsalcedo@gmail.com>
2023-09-19 15:39:18 -0500

    Corregir UI
    +funcionalidad volver al menu principal
    +algunas partes de la interfaz son responsive, esto se mejorara en futuras tareas

---
+++ Assets/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void ChangeScene(string MainMenu)
+    {
+        SceneManager.LoadScene(MainMenu);
+    }
+}

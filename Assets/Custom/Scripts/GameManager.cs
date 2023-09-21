cb8de425ac93e48a15e41ca9609008b5f3888a66
Eduardo De Jesús Mogollón Salcedo <eduardomogollonsalcedo@gmail.com>
2023-09-21 11:15:25 -0500

    importacion de LimpiarEstatuas() en GameManager.cs
    se añadio una funcion redundante para acceder a la funcion LimpiarEstatuas, se llama Limpiar, para acceder desde el GameManager, esta decision fue por que el codigo de Experience toggler usaba "this", por ende no se puede mover de ese objeto(el codigo), otra funcionalidad requeria que estuviera activo el objeto cuando sucediera algo en especifico.

--- Assets/Custom/Scripts/GameManager.cs
+++ Assets/Custom/Scripts/GameManager.cs
@@ -3,8 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public ExperienceToggler experienceToggler;
     public void ChangeScene(string MainMenu)
     {
         SceneManager.LoadScene(MainMenu);
+        
     }
+
+    public void Limpiar()
+    {
+        experienceToggler.LimpiarEstatuas();
+    }
+    
 }

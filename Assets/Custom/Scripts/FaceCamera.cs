0d7a8087fbb4322bde343e3517839dbd54501d33
HennysTous <tf2hennys@gmail.com>
2023-10-19 11:47:58 -0500

    Face camera Script changed

--- Assets/Custom/Scripts/FaceCamera.cs
+++ Assets/Custom/Scripts/FaceCamera.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class FaceCamera : MonoBehaviour
 {
     [SerializeField] Transform camPos;
-    [SerializeField] Transform panel;
+    [SerializeField] Transform panel1;
+    [SerializeField] Transform panel2;
 
     private void Start()
     {
@@ -15,6 +16,8 @@ public class FaceCamera : MonoBehaviour
 
     void Update()
     {
-        transform.rotation = Quaternion.LookRotation(new Vector3(camPos.position.x - panel.position.x, 0 , camPos.position.z - panel.position.z), Vector3.up);
+        transform.rotation = Quaternion.LookRotation(new Vector3(camPos.position.x - panel1.position.x, 0 , camPos.position.z - panel1.position.z), Vector3.up);
+
+        transform.rotation = Quaternion.LookRotation(new Vector3(camPos.position.x - panel2.position.x, 0, camPos.position.z - panel2.position.z), Vector3.up);
     }
 }

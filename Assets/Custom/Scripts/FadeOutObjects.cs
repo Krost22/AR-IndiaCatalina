e855270fa948b60b46676e12de27340e995c56a5
DardilaC <dardilac18@gmail.com>
2023-12-22 10:30:51 -0500

    Modificacion Script FadeOut
    * eliminacion de linea de codigo que causaba problema

--- Assets/Custom/Scripts/FadeOutObjects.cs
+++ Assets/Custom/Scripts/FadeOutObjects.cs
@@ -59,7 +59,6 @@ public class FadeOutObjects : MonoBehaviour
                     m.SetFloat("_Mode", 2);
                     m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
                     m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                    m.SetInt("_ZWrite", 0);
                     m.DisableKeyword("_ALPHATEST_ON");
                     m.EnableKeyword("_ALPHABLEND_ON");
                     m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
